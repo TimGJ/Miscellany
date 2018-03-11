d85e1045de41d872fd2ac7c14beba97ac2094899
TimGJ <tim@greening-jackson.com>
2018-03-11 20:35:08 +0000

    Sunday evening. Now with added Bokeh goodness.

--- DayGantt.py
+++ DayGantt.py
@@ -8,10 +8,8 @@ This script plays with the display of such data.
 
 import datetime
 import itertools
-import random
 import logging
 from bokeh.plotting import figure, show, output_file
-import pandas as pd
 
 class Event:
     """
@@ -21,7 +19,6 @@ class Event:
 
     def __init__(self, description, owner):
         """
-
         :param description: What the event is (string)
         :param owner: What initiates the event e.g. cron (Owner object)
         :param time: When does it fire (datetime.time)
@@ -35,8 +32,14 @@ class Event:
         return "[{}] Event: {} Owner: {} Occurrences: {}".format(self.id, self.description, self.owner, len(self.schedule))
 
     def __lt__(self, other):
-        if self.owner == other.owner:
-            return self.description < other.description
+        if self.schedule and other.schedule:
+            if self.schedule[0].start == other.schedule[0].start:
+                if self.owner == other.owner:
+                    return self.description < other.description
+                else:
+                    return self.owner < other.owner
+            else:
+                return self.schedule[0].start < other.schedule[0].start
         else:
             return self.owner < other.owner
 
@@ -61,6 +64,7 @@ class Event:
                 logging.warning('Event {} already has an entry for {}. Ignoring'.format(self.description, time))
             else:
                 self.schedule.append(Instance(time, duration))
+                self.schedule.sort()
         else:
             logging.error('Schedule entries must be of type datetime.datetime')
 
@@ -89,20 +93,19 @@ class Instance:
 class Owner:
     """
     The owner of an event. Colours and shapes are for plotting on
-    the timeline using matplotlib
+    the timeline using Bokeh
     """
-    colours = itertools.cycle("rybgcm")
-    shapes = itertools.cycle("ov<>^sD")
+    colours = itertools.cycle(["red", "yellow", "green", "blue", "cyan", "magenta"])
     counter = itertools.count()
 
     def __init__(self, name, rank=100):
         self.id = next(Owner.counter)
         self.name = name
         self.rank = rank
-        self.marker = next(Owner.colours)+next(Owner.shapes)
+        self.colour = next(Owner.colours)
 
     def __repr__(self):
-        return "[{}] {} {} {}".format(self.id, self.name, self.rank, self.marker)
+        return "[{}] {} {}".format(self.id, self.name, self.rank)
 
     def __lt__(self, other):
         if self.rank == other.rank:
@@ -110,21 +113,46 @@ class Owner:
         else:
             return self.rank < other.rank
 
+def PlotEvents(events, filename, title):
+    AspectRatio = 16/9
+    EventHeight = 50
+    TitleHeight = 50
+    EventSizeRatio = 1/3
 
-snowy = Owner('Snowy the Wonderdog')
-events = [Event(e, snowy) for e in ['Bury bone', 'Attack postman', 'Chase squirrel', 'Dig bone up again']]
+    events = sorted(events)
+    output_file(filename, title=title)
 
-for index, event in enumerate(events):
-    for hour in range(8,24,4):
-        for minute in range(0,60,30):
-            event.AddInstance(datetime.time(hour+index, minute), datetime.timedelta(minutes=15*(index+1)))
+    height = int(TitleHeight + EventHeight * len(events))
+    width  = int(height*AspectRatio)
+    p = figure(plot_width=width, plot_height=height, title=title,
+               x_axis_type="datetime", y_range=[event.description for event in reversed(events)])
+    size = EventHeight*EventSizeRatio
+    for index, event in enumerate(reversed(events)):
+        y = index + 0.5
+        for instance in event.schedule:
+            if instance.duration:
+                p.hbar(left=instance.start, right=instance.end, y=y, height=EventSizeRatio,
+                       line_color="black", fill_color=event.owner.colour,
+                       alpha=0.75, legend=event.owner.name)
+            else:
+                p.diamond(y=y, x=instance.start, line_color="black", fill_color=event.owner.colour, size=size,
+                          alpha=0.75, legend=event.owner.name)
+    show(p)
 
-output_file('rectangles.html')
+if __name__ == '__main__':
 
-p = figure(plot_width=1000, plot_height=100*len(events), title="Scheduled Events",
-           x_axis_type="datetime", y_range=[event.description for event in reversed(events)])
-for index, event in enumerate(reversed(events)):
-    y = index + 0.5
-    for instance in event.schedule:
-        p.hbar(y=y, left=instance.start, right=instance.end, height=0.1, line_color="black", fill_color="firebrick", alpha=0.5)
-show(p)
+    snowy = Owner('Snowy the Wonderdog')
+    tim = Owner('The Fat Controller')
+    ming = Owner('Ming the merciless')
+    events  = [Event(e, snowy) for e in ['Attack postman', 'Bury bone', 'Chase squirrel', 'Dig bone up again', 'Eat dinner']]
+    events += [Event(e, tim) for e in ['Arcati', 'Baking', 'Cooking']]
+    for index, event in enumerate(events):
+        for hour in range(8,16,4):
+            for minute in range(0,60,30):
+                event.AddInstance(datetime.time(hour+index, minute), datetime.timedelta(minutes=15*(index)))
+                event.AddInstance(datetime.time(hour, minute))
+    events += [Event(e, ming) for e in ['World Domination']]
+    events[-1].AddInstance(datetime.time(3,0))
+    events[-1].AddInstance(datetime.time(4,0), datetime.timedelta(hours=2))
+    events[-2].AddInstance(datetime.time(5,30), datetime.timedelta(minutes=30))
+    PlotEvents(events, 'events.html', 'Stuff happening')
